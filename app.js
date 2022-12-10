132015a8d0a48fef01083f4b42152b464119558a
AlekseySamokhin <lel.dude01@gmail.com>
2022-12-10 20:12:24 +0300

    feat: add cors

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+
 const routes = require("./routes");
+const localCongig = require("./config");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: localCongig,
+  })
+);
 
 app.use(express.urlencoded({ extended: true }));
 
