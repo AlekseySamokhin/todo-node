55ff14a9f0ea8c01b60089e16386d9b5859ea901
AlekseySamokhin <lel.dude01@gmail.com>
2022-12-14 17:26:39 +0300

    refactor: code

--- controllers/getTodos.js
+++ controllers/getTodos.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const TodoModel = require("../models/Todos");
 
-router.get("/", async (req, res) => {
+router.get("/", async (res) => {
   console.log("get todos");
   try {
     const todolist = await TodoModel.find({});
